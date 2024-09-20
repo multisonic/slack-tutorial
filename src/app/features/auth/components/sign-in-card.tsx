14b19dae852cf61b5b31a7b0294366fefbc61741
Michael Zaleski <me@mikezaleski.com>
2024-09-20 10:30:30 -0400

    Auth Flow: continue login component - add google and github button elements

--- src/app/features/auth/components/sign-in-card.tsx
+++ src/app/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,5 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,6 +10,7 @@ import {
 } from "@/components/ui/card";
 
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 
 export const SignInCard = () => {
   return (
@@ -40,6 +43,35 @@ export const SignInCard = () => {
             Continue
           </Button>
         </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle className="size-5 absolute top-3 left-2.5" />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute top-3 left-2.5" />
+            Continue with GitHub
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <span className="text-sky-700 hover:underline cursor-pointer">
+            Sign up
+          </span>
+        </p>
       </CardContent>
     </Card>
   );
